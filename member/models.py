14bf4a18fd590b57b81228165f0a37096c604977
91kimsam <91kimsam@naver.com>
2022-06-23 12:05:09 +0900

    Django member join views, template 수정 -  클라이언트측/서버측 유효성 검사, Member 객체 생성후 디비 저장 가입 완료시 ok 페이지로 전환

--- member/models.py
+++ member/models.py
@@ -1,3 +1,22 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
+
+# Django에서 폼 데이터 다루는 방법은 크게 2가지 Model ModelForm
+# member 테이블의 구조와 유사하게  member 모델정의
+class Member(models.Model):
+    id=models.AutoField(primary_key=True)
+    userid=models.CharField(max_length=18, unique=True)
+    passwd=models.CharField(max_length=18)
+    name=models.CharField(max_length=5)
+    email=models.CharField(max_length=100)
+    regdate=models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        db_table = 'member'  # 생성할 테이블 이름 지정
+        ordering = ['-regdate']  # 정렬기준 지정
+
+    def __str__(self):
+        return self.userid
